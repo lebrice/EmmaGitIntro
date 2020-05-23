c32149aad29799395aa4369a292780f9c73fd1cf
Fabrice Normandin <fabrice.normandin@gmail.com>
2020-05-23 12:47:04 -0400

    added a class Cat, with Bob and Charlie

--- foo.py
+++ foo.py
@@ -2,3 +2,11 @@ def foo(a: int, b: int) -> int:
     return a + a ** b + a * b
 
 print("foo(1, 2) = ", foo(1,2))
+
+class Cat:
+    def __init__(self, name: str="Bob"):
+        self.name = name
+        print("Hello! My name is", self.name)
+
+bob = Cat()
+charlie = Cat(name="Charlie")
